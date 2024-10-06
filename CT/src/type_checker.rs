2448d0367f9e4709160001f9cfe6636119fa857c
Utkarsh Maurya <projects.utkarshMaurya@gmail.com>
2024-10-07 03:13:45 +0530

    added: type_checker.rs template

---
+++ CT/src/type_checker.rs
@@ -0,0 +1,9 @@
+use anyhow::{Context, Result};
+
+// Function to perform type checking
+pub fn type_check(parsed_data: Vec<String>) -> Result<Vec<String>> {
+    // For demonstration, we will just return the same data
+    // In a real implementation, you'd check for types here
+    Ok(parsed_data)
+}
+
